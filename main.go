9c3d51407c373c8032d6f6a8e350492879265744
lzhaoyang <575563079@qq.com>
2020-04-09 00:34:50 +0800

    init the repo

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+//接受客户端
+type InboundCon struct {
+	ClinetIp   string
+	ClientPort int
+	Protocol   string
+}
+
+//服务端
+type OutBoundCon struct {
+	ServerIp   string
+	ServerPort int
+	Protocol   string
+}
+
+func main() {
+
+}
